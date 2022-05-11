3d37c53ae4480a0da26c6a3d1d3f4ab399fa9d02
Victor Mamede <victoraugustomamede@hotmail.com>
2022-05-11 19:49:22 -0300

    added chara

--- components/message_board.js
+++ components/message_board.js
@@ -8,6 +8,7 @@ import {
   HStack,
   Icon,
   Divider,
+  VStack,
 } from '@chakra-ui/react';
 import Image from 'next/image';
 import dog from '../assets/dog.svg';
@@ -17,58 +18,71 @@ import { FaComment } from 'react-icons/fa';
 export default function MessageBoard() {
   return (
     <Container maxW='container.lg' p={3}>
-      <HStack>
-        <Icon as={FaComment} />
-        <Box boxShadow={'lg'} p={3} mb={3}>
-          <HStack spacing={3} mb={3}>
-            <Avatar size={'sm'} src={'/Frisk.webp'} />
-            <Text fontWeight={'bold'}>Frisk</Text>
-            <Text color='gray.600'> Sobre o livro - {mockBooks[0].title}</Text>
-          </HStack>
-          <Text>Muito empolgante, chorei muito no final</Text>
-        </Box>
-      </HStack>
-      <HStack>
-        <Icon as={FaComment} />
-        <Box boxShadow={'lg'} p={3} mb={3}>
-          <HStack spacing={3} mb={3}>
-            <Avatar size={'sm'} src={'/Undyne.webp'} />
-            <Text fontWeight={'bold'}>Undyne</Text>
-            <Text color='gray.600'> Sobre o livro - {mockBooks[1].title}</Text>
-          </HStack>
-          <Text>
-            Deixou muito a desejar, as lutas não são empolgantes, o sistema de
-            magia é fraco e os personagens não são nada carismáticos
-          </Text>
-        </Box>
-      </HStack>
-      <HStack>
-        <Icon as={FaComment} />
-        <Box boxShadow={'lg'} p={3} mb={3}>
-          <HStack spacing={3} mb={3}>
-            <Avatar size={'sm'} src={'/Sans.webp'} />
-            <Text fontWeight={'bold'}>Sans</Text>
-            <Text color='gray.600'> Sobre o livro - {mockBooks[2].title}</Text>
-          </HStack>
-          <Text fontFamily={'"Comic Sans", cursive, sans-serif'}>
-            Bom, vou recomendar para o meu irmão
-          </Text>
-        </Box>
-      </HStack>
-      <HStack>
-        <Icon as={FaComment} />
-        <Box boxShadow={'lg'} p={3} mb={3}>
-          <HStack spacing={3} mb={3}>
-            <Avatar size={'sm'} src={'/papyrus.png'} />
-            <Text fontWeight={'bold'}>Papyrus</Text>
-            <Text color='gray.600'> Sobre o livro - {mockBooks[2].title}</Text>
-          </HStack>
-          <Text fontFamily={'Papyrus,fantasy'} fontSize={'4xl'}>
-            TODAS AS LETRAS E PERSONAGENS ESTAVAM DE CABEÇA PARA BAIXO E TINHA
-            98 VOLUMES
-          </Text>
-        </Box>
-      </HStack>
+      <VStack spacing={4} alignItems='flex-start'>
+        <HStack>
+          <Icon as={FaComment} />
+          <Box boxShadow={'lg'} p={3} mb={3}>
+            <HStack spacing={3} mb={3}>
+              <Avatar size={'sm'} src={'/Frisk.webp'} />
+              <Text fontWeight={'bold'}>Frisk</Text>
+              <Text color='gray.600'>Sobre o livro - {mockBooks[0].title}</Text>
+            </HStack>
+            <Text>Muito empolgante, chorei muito no final</Text>
+          </Box>
+        </HStack>
+        <HStack>
+          <Icon as={FaComment} />
+          <Box boxShadow={'lg'} p={3} mb={3}>
+            <HStack spacing={3} mb={3}>
+              <Avatar size={'sm'} src={'/Undyne.webp'} />
+              <Text fontWeight={'bold'}>Undyne</Text>
+              <Text color='gray.600'>Sobre o livro - {mockBooks[1].title}</Text>
+            </HStack>
+            <Text>
+              Deixou muito a desejar, as lutas não são empolgantes, o sistema de
+              magia é fraco e os personagens não são nada carismáticos
+            </Text>
+          </Box>
+        </HStack>
+        <HStack>
+          <Icon as={FaComment} />
+          <Box boxShadow={'lg'} p={3} mb={3}>
+            <HStack spacing={3} mb={3}>
+              <Avatar size={'sm'} src={'/Sans.webp'} />
+              <Text fontWeight={'bold'}>Sans</Text>
+              <Text color='gray.600'>Sobre o livro - {mockBooks[2].title}</Text>
+            </HStack>
+            <Text fontFamily={'"Comic Sans", cursive, sans-serif'}>
+              Bom, vou recomendar para o meu irmão
+            </Text>
+          </Box>
+        </HStack>
+        <HStack>
+          <Icon as={FaComment} />
+          <Box boxShadow={'lg'} p={3} mb={3}>
+            <HStack spacing={3} mb={3}>
+              <Avatar size={'sm'} src={'/papyrus.png'} />
+              <Text fontWeight={'bold'}>Papyrus</Text>
+              <Text color='gray.600'>Sobre o livro - {mockBooks[2].title}</Text>
+            </HStack>
+            <Text fontFamily={'Papyrus,fantasy'} fontSize={'4xl'}>
+              TODAS AS LETRAS E PERSONAGENS ESTAVAM DE CABEÇA PARA BAIXO E TINHA
+              98 VOLUMES
+            </Text>
+          </Box>
+        </HStack>
+        <HStack>
+          <Icon as={FaComment} />
+          <Box boxShadow={'lg'} p={3} mb={3}>
+            <HStack spacing={3} mb={3}>
+              <Avatar size={'sm'} src={'/Chara.webp'} />
+              <Text fontWeight={'bold'}>Chara</Text>
+              <Text color='gray.600'>Sobre o livro - {mockBooks[3].title}</Text>
+            </HStack>
+            <Text>No final o protagonista morre</Text>
+          </Box>
+        </HStack>
+      </VStack>
       <Divider my={5} />
     </Container>
   );
