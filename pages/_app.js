770602c95f5493e3cd8e76ca309aebeb1abac472
Victor Mamede <victoraugustomamede@hotmail.com>
2022-05-11 19:37:40 -0300

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import '../style/index.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>The book club</title>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
